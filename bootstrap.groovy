f55e12414343a9f5ccedaba63cf8104b949dd4f0
lexandro <lexandro2000@gmail.com>
2015-05-23 15:23:29 +0200

    Adding docker container stop/remove before starting a new one

--- bootstrap.groovy
+++ bootstrap.groovy
@@ -149,8 +149,10 @@ job(deployJobName) {
         project / buildWrappers / 'org.jvnet.hudson.plugins.SSHBuildWrapper' {
             siteName 'lexandro@imaginarium.lexandro.com:22'
             postScript """
+docker stop imaginarium
+docker rm imaginarium
 docker pull lexandro/imaginarium
-docker run -dt lexandro/imaginarium java -Djava.security.egd=file:/dev/urandom -jar imaginarium.jar
+docker run -dt --name 'imaginarium' -p 80:8080 lexandro/imaginarium java -Djava.security.egd=file:/dev/urandom -jar imaginarium.jar
 """
         }
     }
